86132d585385b6bf69bb4863929e10e976808917
Lapadus Raluca <raluca.lapadus13@gmail.com>
2024-01-04 22:34:31 +0200

    Added filter by priority and sort by type

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -31,28 +31,63 @@ namespace TaskBuddy.Pages
         [BindProperty(SupportsGet = true)]
         public string? Description  {get; set; }
 
-        public string DateSort { get; set; }
+        [BindProperty]
+        public List<string> SelectedPrio { get; set; }
 
-
-        public async Task OnGetAsync(string sortOrder)
+        public async Task OnPostAsync()
         {
-            DateSort = sortOrder == "Date" ? "date_desc" : "Date";
-
-            var taskuri = from task in _context.Taskuri
-            select task;
-
-            List<Taskuri> TaskNotification = taskuri.ToList();
-
-            switch (sortOrder)
+            // SelectedPrio will contain the values of checked checkboxes
+            if (SelectedPrio != null && SelectedPrio.Count > 0)
             {
+                // Filter the list based on selected priorities
+                var taskuri = from task in _context.Taskuri
+                              select task;
 
-                case "Date":
-                    taskuri = taskuri.OrderBy(s => s.CreatedAt);
-                    break;
-                case "date_desc":
-                    taskuri = taskuri.OrderByDescending(s => s.CreatedAt);
-                    break;
 
+                taskuri = taskuri.Where(item => SelectedPrio.Contains(item.Priority));
+                Taskuri = await taskuri.ToListAsync();
+            }
+        }
+
+        public string DateSort { get; set; } = "desc";
+        public string TagSort { get; set; } = "asc";
+
+
+        public async Task OnGetAsync(string dateSort, string typeSort)
+        {
+            var taskuri = from task in _context.Taskuri
+                          select task;
+
+            if (!string.IsNullOrEmpty(dateSort))
+            {
+                DateSort = dateSort == "asc" ? "desc" : "asc";
+                switch (dateSort)
+                {
+
+                    case "asc":
+                        taskuri = taskuri.OrderBy(s => s.CreatedAt);
+                        break;
+                    case "desc":
+                        taskuri = taskuri.OrderByDescending(s => s.CreatedAt);
+                        break;
+
+                }
+            }
+
+            if (!string.IsNullOrEmpty(typeSort))
+            {
+                TagSort = typeSort == "asc" ? "desc" : "asc";
+                switch (typeSort)
+                {
+
+                    case "asc":
+                        taskuri = taskuri.OrderBy(s => s.Type);
+                        break;
+                    case "desc":
+                        taskuri = taskuri.OrderByDescending(s => s.Type);
+                        break;
+
+                }
             }
 
             if (!string.IsNullOrEmpty(SearchString))
@@ -61,7 +96,8 @@ namespace TaskBuddy.Pages
             }
             Taskuri = await taskuri.ToListAsync();
 
-   
+
+            List<Taskuri> TaskNotification = taskuri.ToList();
             foreach (Taskuri myTask in TaskNotification)
             if (myTask.IsDeadlineNear())
             {
